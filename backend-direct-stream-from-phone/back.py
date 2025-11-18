bec08f433f3eec3f5836c1950710829d8c9431e4
Aleksa Dinic <aleksa.dinic@magmadev.co>
2025-11-18 10:16:43 +0100

    added requierments and mapped port

--- backend-direct-stream-from-phone/back.py
+++ backend-direct-stream-from-phone/back.py
@@ -185,4 +185,6 @@ def status():
         })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
