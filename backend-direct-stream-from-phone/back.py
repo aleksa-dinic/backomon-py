b5e2ca64a49e60934dc5824fdd9b912be83b4893
Aleksa Dinic <aleksa.dinic@magmadev.co>
2025-11-18 20:45:21 +0100

    .

--- backend-direct-stream-from-phone/back.py
+++ backend-direct-stream-from-phone/back.py
@@ -92,7 +92,7 @@ def generate_frames():
 @app.route('/')
 def index():
     """Render the main page"""
-    return render_template('index.html')
+    return render_template('index-2.html')
 
 @app.route('/start_camera', methods=['POST'])
 def start_camera():
